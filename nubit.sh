2aefec46329af8f806f10ed047bbb014bc751be3
Ivan Shumeiko <i.shumeiko@amarkets.com>
2024-06-26 20:39:00 +0300

    fix: fix bash script

--- nubit.sh
+++ nubit.sh
@@ -46,42 +46,37 @@ print_message "Пакеты успешно обновлены." "32"
 
 # Установка необходимых пакетов
 print_message "Установка необходимых пакетов..." "32"
-if ! sudo DEBIAN_FRONTEND=noninteractive apt install -y curl git wget build-essential jq screen; then
+if ! sudo DEBIAN_FRONTEND=noninteractive apt install -y curl wget build-essential jq screen; then
     print_message "Ошибка при установке пакетов. Прерывание скрипта." "31"
     exit 1
 fi
 print_message "Пакеты успешно установлены." "32"
 
-# Запуск screen сессии и выполнение команды установки ноды
-print_message "Запуск screen сессии и установка ноды..." "32"
-if ! screen -S nubit -dm bash -c 'curl -sL https://nubit.sh | bash'; then
+# Создание screen сессии с именем nubit
+print_message "Запуск screen сессии..." "32"
+if screen -S nubit -dm; then
+    print_message "Screen сессия успешно запущена." "32"
+else
     print_message "Ошибка при запуске screen сессии. Прерывание скрипта." "31"
     exit 1
 fi
-print_message "Screen сессия успешно запущена для установки ноды." "32"
 
-# Ожидание 3 минут перед проверкой логов
-print_message "Ожидание 3 минут перед проверкой логов..." "93"
-countdown 180 93
-
-# Проверка появления логов с сообщениями INFO
-print_message "Ожидание появления логов с сообщениями INFO..." "93"
-while ! screen -list | grep -q "nubit"; do
-    print_message "Screen сессия не найдена. Увеличиваем время ожидания на 1 минуту и продолжаем..." "93"
-    countdown 60 93
-done
-
-screen -S nubit -X hardcopy .nubit.log
-if grep -q "INFO" .nubit.log; then
-    print_message "Сообщения INFO найдены. Продолжаем..." "32"
+# Установка ноды в созданной screen сессии
+print_message "Установка ноды..." "32"
+if curl -sL https://nubit.sh | bash$(printf \\r); then
+    print_message "Команда установки ноды успешно отправлена в screen сессию." "32"
 else
-    print_message "Логи INFO не найдены после ожидания. Прерывание скрипта." "31"
+    print_message "Ошибка при отправке команды установки ноды. Прерывание скрипта." "31"
     exit 1
 fi
 
+# Ожидание 3 минут перед проверкой логов
+print_message "Ожидание 3 минут перед ..." "93"
+countdown 180 93
+
 # Автоматический выход из screen сессии
 print_message "Автоматический выход из screen сессии..." "32"
-if ! screen -S nubit -X quit; then
+if ! screen -S nubit -X detach; then
     print_message "Ошибка при завершении screen сессии." "31"
 else
     print_message "Screen сессия успешно завершена." "32"
