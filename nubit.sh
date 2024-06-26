3ea5cb185255d181cb5eccc2ff6c8677338596c2
Ivan Shumeiko <i.shumeiko@amarkets.com>
2024-06-26 16:17:40 +0300

    chore: add script for nubit

--- nubit.sh
+++ nubit.sh
@@ -1,65 +1,92 @@
 #!/bin/bash
 
-# Функция для вывода обратного отсчета
+# Функция для вывода обратного отсчета с указанием цвета
 function countdown {
     local counter=$1
+    local color=$2
 
     while [ $counter -gt 0 ]; do
-        echo -ne "Ожидание завершения установки... $counter сек\r"
+        echo -ne "\e[93mОжидание завершения установки... $counter сек\e[0m\r"
         sleep 1
         ((counter--))
     done
     echo -ne "\n"
 }
 
+# Функция для вывода сообщения с указанием цвета
+function print_message {
+    local message=$1
+    local color=$2
+
+    echo -e "\e[${color}m${message}\e[0m"
+}
+
 # Установка debconf-utils для предварительной настройки
-echo "Установка debconf-utils для предварительной настройки..."
+print_message "Установка debconf-utils для предварительной настройки..." "32"
 sudo apt-get install -y debconf-utils
 
 # Предварительная настройка для автоматического перезапуска сервисов
+print_message "Настройка debconf для автоматического режима" "32"
 echo "debconf debconf/frontend select Noninteractive" | sudo debconf-set-selections
 echo "debconf debconf/reconfigure boolean true" | sudo debconf-set-selections
 
 # Обновление пакетов
-echo "Обновление пакетов..."
-sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+print_message "Обновление пакетов..." "32"
+if sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y; then
+    print_message "Пакеты успешно обновлены." "32"
+else
+    print_message "Ошибка при обновлении пакетов. Прерывание скрипта." "31"
+    exit 1
+fi
 
 # Установка необходимых пакетов
-echo "Установка необходимых пакетов..."
-sudo DEBIAN_FRONTEND=noninteractive apt install -y curl git wget build-essential jq screen
+print_message "Установка необходимых пакетов..." "32"
+if sudo DEBIAN_FRONTEND=noninteractive apt install -y curl git wget build-essential jq screen; then
+    print_message "Пакеты успешно установлены." "32"
+else
+    print_message "Ошибка при установке пакетов. Прерывание скрипта." "31"
+    exit 1
+fi
 
 # Запуск screen сессии и выполнение команды установки ноды
-echo "Запуск screen сессии и установка ноды..."
-screen -S nubit -dm bash -c 'curl -sL https://nubit.sh | bash'
+print_message "Запуск screen сессии и установка ноды..." "32"
+if screen -S nubit -dm bash -c 'curl -sL https://nubit.sh | bash'; then
+    print_message "Screen сессия успешно запущена для установки ноды." "32"
+else
+    print_message "Ошибка при запуске screen сессии. Прерывание скрипта." "31"
+    exit 1
+fi
 
-# Ожидание 3 минуты перед проверкой логов
-echo "Ожидание 3 минуты перед проверкой логов..."
+# Ожидание 3 минут перед проверкой логов
+print_message "Ожидание 3 минут перед проверкой логов..." "93"
 countdown 180
 
-# Переменная для хранения времени ожидания
-wait_time=180  # Исходное время ожидания 180 секунд (3 минуты)
-
-# Ожидание появления логов с сообщениями INFO
-echo "Ожидание появления логов с сообщениями INFO..."
-while ! screen -S nubit -Q select . > /dev/null 2>&1; do
-    if screen -S nubit -X stuff "tail -n 20 /dev/null | grep INFO\n"; then
-        echo "Сообщения INFO найдены. Продолжаем..."
-        break
-    else
-        echo "Логи INFO не найдены. Увеличиваем время ожидания на 1 минуту и продолжаем..."
-        wait_time=$((wait_time + 60))  # Увеличиваем время ожидания на 1 минуту
-        countdown 60  # Обратный отсчет 60 секунд (1 минута)
-    fi
+# Проверка появления логов с сообщениями INFO
+print_message "Ожидание появления логов с сообщениями INFO..." "93"
+while ! screen -S nubit -X hardcopy .nubit.log; do
+    print_message "Логи INFO не найдены. Увеличиваем время ожидания на 1 минуту и продолжаем..." "93"
+    countdown 60
 done
 
+if grep -q "INFO" .nubit.log; then
+    print_message "Сообщения INFO найдены. Продолжаем..." "32"
+else
+    print_message "Логи INFO не найдены после ожидания. Прерывание скрипта." "31"
+    exit 1
+fi
+
 # Автоматический выход из screen сессии
-echo "Автоматический выход из screen сессии..."
-screen -S nubit -X stuff "exit\n"
+print_message "Автоматический выход из screen сессии..." "32"
+if screen -S nubit -X quit; then
+    print_message "Screen сессия успешно завершена." "32"
+else
+    print_message "Ошибка при завершении screen сессии." "31"
+fi
 
 # Сохранение сид-фразы
-echo "Сохранение сид-фразы..."
+print_message "Сохранение сид-фразы..." "33"
 cat $HOME/nubit-node/mnemonic.txt
 
 # Удаление скрипта после успешного выполнения
-echo "Удаление скрипта..."
+print_message "Удаление скрипта..." "33"
 rm -- "$0"
